b2b36f72d57852265ab6d6e0755981d24494b1ec
w00j00ng <magoswj@gmail.com>
2021-01-05 00:59:15 +0900

    listener modified 2

--- test4_listener_console.py
+++ test4_listener_console.py
@@ -1,25 +1,66 @@
 import os
 
-helper = ["!help"]
-finish = ["!quit", "!exit"]
-show = ["!show"]
-delete = ["!delete"]
-save = ['!save']
-clear = ['!clear']
-whoami = ['!Who are you', '!who are you', '!Who are you?', '!who are you?'\
-          , "!your story"\
-          , "!Where are you", "!where are you"]
-            
-responsor = ['!I love you']
+def clear_f():
+    cleaner = lambda: os.system("cls")
+    cleaner()
 
-orderList = [helper, finish, show, delete, save, clear]
+def help_f():
+    print("\nORDER LIST")
+    for order in orderList:
+        for i in order:
+            print(i, end = " ")
+    print()
+    print("====================================================")
 
-story = []
+def show_f():
+    if story:
+        print("\n<Your Story>")
+        for i in story:
+            print(i)
+    else:
+        print("I have nothing to tell you")
+
+def delete_f():
+    story.clear()
+    print()
+    print("Your Story Deleted \n")
+    print("ORDER LIST")
+    for order in orderList:
+        for i in order:
+            print(i, end = " ")
+    print()
+    print("====================================================")
+    print('Tell me "!help" if you need any help')
+    print("====================================================")
+
+def save_f():
+    filename = input("TITLE: ")
+    outFp = open("./"+filename+".txt", "w", encoding = 'ANSI')
+        
+    for i in story:
+        outFp.writelines(i+"\n")
+        
+    outFp.close()
+    print("Your Story Saved \n")
 
 
 print("Hello. I am your listener")
 print('Tell me "!help" if you need any help')
 print("====================================================")
+
+helpStr = ["!help"]
+finishStr = ["!quit", "!exit"]
+showStr = ["!show"]
+deleteStr = ["!delete"]
+saveStr = ['!save']
+clearStr = ['!clear']
+whoamiStr = ['!Who are you', '!who are you', '!Who are you?', '!who are you?'\
+          , "!your story"\
+          , "!Where are you", "!where are you"]
+special1Str = ['!I love you']
+orderList = [helpStr, finishStr, showStr, deleteStr, saveStr, clearStr]
+
+story = []
 
 while True:
     words = input()
@@ -29,65 +70,31 @@ while True:
         continue
         
     else:
-        if words in finish:
+        story.pop()
+        if words in finishStr:
             break
             
-        elif words in clear:
-            story.pop()
-            cleaner = lambda: os.system("cls")
-            cleaner()
+        elif words in clearStr:
+            clear_f()
             
-        elif words in helper:
-            story.pop()
-            print("\nORDER LIST")
-            for order in orderList:
-                for i in order:
-                    print(i, end = " ")
-            print()
-            print("====================================================")
+        elif words in helpStr:
+            help_f()
             
-        elif words in show:
-            story.pop()
-            if story:
-                print("\n<Your Story>")
-                for i in story:
-                    print(i)
-            else:
-                print("I have nothing to tell you")
+        elif words in showStr:
+            show_f()
                 
-        elif words in delete:
-            story = []
-            print()
-            print("Your Story Deleted \n")
-            print("ORDER LIST")
-            for order in orderList:
-                for i in order:
-                    print(i, end = " ")
-            print()
-            print("====================================================")
-            print('Tell me "!help" if you need any help')
-            print("====================================================")
+        elif words in deleteStr:
+            delete_f()
             
-        elif words in save:
-            story.pop()
-            
-            filename = input("TITLE: ")
-            outFp = open("./"+filename+".txt", "w", encoding = 'utf-8')
-            
-            for i in story:
-                outFp.writelines(i+"\n")
-            
-            outFp.close()
-            print("Your Story Saved \n")
+        elif words in saveStr:
+            save_f()
 
-        elif words in whoami:
-            story.pop()
+        elif words in whoamiStr:
             print("My name is Raven")
             print("w00j00ng351@gmail.com")
         
-        elif words in responsor:
-            story.pop()
+        elif words in special1Str:
             print("I love you too")
+
         else:
             continue
-    
