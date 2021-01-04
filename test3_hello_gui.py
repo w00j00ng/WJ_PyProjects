50cc2d2c11987314752c596e1a6a421c5dbf89dc
w00j00ng <magoswj@gmail.com>
2021-01-05 08:39:08 +0900

    gui modified 1

--- test3_hello_gui.py
+++ test3_hello_gui.py
@@ -2,15 +2,34 @@ from tkinter import *
 
 window = Tk()
 
+window.title("The Title")
 window.geometry("400x400")
-
-def exit_f():
-    window.destroy()
 
 label1 = Label(window, text = "Hello")
 label1.pack()
 
-button1 = Button(window, text = "Exit", command = exit_f)
-button1.pack()
+txt = Text(window, width = 30, height = 5)
+txt.pack()
+txt.insert(END, "Insert several lines")
+
+e = Entry(window, width = 30)
+e.pack()
+e.insert(0, "Insert one line")
+
+def btn_1_cmd():
+    print(txt.get("1.0", END))
+    print(e.get())
+
+    txt.delete("1.0", END)
+    e.delete(0, END)
+
+def exit_f():
+    window.destroy()
+
+btn1 = Button(window, text = "클릭", command = btn_1_cmd)
+btn1.pack()
+
+btn2 = Button(window, text = "Exit", command = exit_f)
+btn2.pack()
 
 window.mainloop()
